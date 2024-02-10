8c24717d3315ee6e1f3702aa9205695e16fe1ae4
heitzjulien <heitzjulien@pm.me>
2024-02-10 22:49:06 +0100

    chore: Add database configuration and types

---
+++ backend/src/config/database/database.types.ts
@@ -0,0 +1,49 @@
+import {
+  ColumnType,
+  Generated,
+  Insertable,
+  JSONColumnType,
+  Selectable,
+  Updateable,
+} from "kysely";
+
+export interface Database {
+  recipe: RecipeTable;
+  instruction: InstructionTable;
+  ingredient: IngredientTable;
+}
+
+export interface RecipeTable {
+  id: Generated<number>;
+  title: string;
+  image_url: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export type Recipe = Selectable<RecipeTable>;
+export type NewRecipe = Insertable<RecipeTable>;
+export type RecipeUpdate = Updateable<RecipeTable>;
+
+export interface InstructionTable {
+  id: Generated<number>;
+  recipe_id: number;
+  step_number: number;
+  description: string;
+}
+
+export type Instruction = Selectable<InstructionTable>;
+export type NewInstruction = Insertable<InstructionTable>;
+export type InstructionUpdate = Updateable<InstructionTable>;
+
+export interface IngredientTable {
+  id: Generated<number>;
+  recipe_id: number;
+  name: string;
+  quantity: number;
+  unit: string;
+}
+
+export type Ingredient = Selectable<IngredientTable>;
+export type NewIngredient = Insertable<IngredientTable>;
+export type IngredientUpdate = Updateable<IngredientTable>;
