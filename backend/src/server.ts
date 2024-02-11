35279b6d7ef241662a0d63ae98e192c90c4ae043
heitzjulien <heitzjulien@pm.me>
2024-02-11 14:15:33 +0100

    feat(handler): Add HTTP error handlers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction } from "express";
 import cors from "cors";
 import recipeRoute from "./routes/recipeRoute";
 import instructionRoute from "./routes/instructionRoute";
 import ingredientRoute from "./routes/ingredientRoute";
 import { envConfig } from "./config/envConfig";
 import route from "./routes/route";
+import { errorHandler, notFoundHandler } from "./handlers/httpErrorHandlers";
 
 envConfig();
 
@@ -22,9 +23,9 @@ app.use(recipeRoute);
 app.use(instructionRoute);
 app.use(ingredientRoute);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not Found" });
-});
+// Http error handlers
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
