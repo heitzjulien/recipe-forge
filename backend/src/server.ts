5dfc19aafcd110ed05a40b0cd45d0d41d31546a7
heitzjulien <heitzjulien@pm.me>
2024-02-15 14:16:19 +0100

    style(components): component styles directly into Astro file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ import route from "./routes/route";
 envConfig();
 
 const app: Application = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
